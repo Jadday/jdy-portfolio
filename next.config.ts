78a8583bacef4d3d0597593051b65825fd6828db
Jadday <jadday0544@gmail.com>
2025-03-02 21:43:32 +0000

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withMDX from '@next/mdx'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  // Extend your page extensions to include MDX
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // You can add other Next.js configurations here
+}
 
-export default nextConfig;
+export default withMDX({
+  extension: /\.mdx?$/,
+  // Optionally, you can add any MDX-specific configuration here
+  // For example, you can set a remark or rehype plugin if needed.
+  ...nextConfig
+})
