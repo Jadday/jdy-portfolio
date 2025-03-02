454d7716a2f70cc59b5e710d0da2da1424bdf0a0
Jadday <jadday0544@gmail.com>
2025-03-02 22:49:44 +0000

    Fix: blog page missing Promise property

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,18 @@
 // app/blog/[slug]/page.tsx
 import { notFound } from 'next/navigation';
+import { JSX } from 'react';
 
-export default async function BlogPostPage({ params }: { params: { slug: string } }) {
+type PageProps = {
+  params: {
+    slug: string;
+  }
+};
+
+export default async function BlogPostPage({ params }: PageProps): Promise<JSX.Element> {
   if (!params.slug) {
     notFound();
   }
+  // Future asynchronous data fetching can be done here.
   return (
     <div>
       <h1 className="text-3xl font-bold mb-4">Blog Post: {params.slug}</h1>
